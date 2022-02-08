41ef413ba977887d2156dbbafa768e9f458eb640
Gyosic <zpzptls@naver.com>
2022-02-09 02:08:36 +0900

    feat: Add Header

---
+++ daum/src/assest/dummy.js
@@ -0,0 +1,347 @@
+export const favorSch = [
+  {
+    link: "https://search.daum.net/search?w=tot&q=%EC%BD%94%EB%A1%9C%EB%82%9819+%EB%B0%9C%EC%83%9D%ED%98%84%ED%99%A9&DA=NPI",
+    favor: "코로나19 발생현황",
+  },
+  {
+    link: "https://search.daum.net/search?w=tot&q=%EB%B0%95%EC%9E%AC%EB%B2%94+%EC%B6%9C%EC%97%B0%ED%99%95%EC%A0%95&DA=NPI&rtmaxcoll=NNS",
+    favor: "박재범 출연확정",
+  },
+  {
+    link: "https://search.daum.net/search?w=tot&q=%EC%A0%9C%EC%A3%BC%EB%A0%8C%ED%8A%B8%EC%B9%B4&DA=NPT",
+    favor: "제주렌트카",
+  },
+  {
+    link: "https://search.daum.net/search?w=tot&q=%EA%B9%80%EB%8B%A4%EB%AF%B8+%ED%99%94%EB%B3%B4&DA=NPI&rtmaxcoll=NNS",
+    favor: "김다미 화보",
+  },
+  {
+    link: "https://search.daum.net/search?w=tot&q=%EC%96%BC%EC%9D%8C%EC%A0%95%EC%88%98%EA%B8%B0&DA=NPT",
+    favor: "얼음정수기",
+  },
+];
+
+export const services = [
+  { link: "https://cafe.daum.net", service: "카페" },
+  { link: "https://mail.daum.net", service: "메일" },
+  { link: "https://news.daum.net", service: "뉴스" },
+  { link: "https://map.daum.net", service: "지도" },
+  { link: "https://finance.daum.net", service: "증권" },
+  { link: "https://shoppinghow.kakao.com/top", service: "쇼핑" },
+  { link: "https://tv.kakao.com", service: "카카오TV" },
+  { link: "https://webtoon.kakao.com", service: "웹툰" },
+  { link: "https://calendar.kakao.com", service: "캘린더" },
+  { link: "https://brunch.co.kr", service: "브런치" },
+  { link: "https://dic.daum.net", service: "사전" },
+  { link: "https://game.daum.net", service: "게임" },
+  { link: "https://together.kakao.com", service: "같이가치" },
+  { link: "", service: "더보기" },
+];
+
+export const news = [
+  {
+    title: `"옆집 살아도 이웃되려나"..文 양산 사저 현장 가보니`,
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/kukminilbo/20220207171059104jxxi.jpg",
+  },
+  {
+    title: `'아슬아슬' 공중에 떠 있는 거대한 물체, 시민들 안전 누가 책임 지나 [제보영상]`,
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/YTN/20220207184618029twbn.jpg",
+  },
+  {
+    title: `황대헌,'최고의 플레이가 어처구니 없는 실격 사유로 판정'`,
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/section/oc/307b1669e3c145a3b3d73f71dc900297",
+  },
+  {
+    title: `[메이킹] 아트센터에 모인 주연네와 재희네 이미지 메이킹을 위한 촬영 l 공작도시 비하인드 27`,
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://thumb.kakaocdn.net/dna/kamp/source/rv4q1ufbm6ts1vdv78nx9d97r/thumbs/thumb.jpg?credential=TuMuFGKUIcirOSjFzOpncbomGFEIdZWK&amp;expires=33201140865&amp;signature=MU%2Bj%2FgTYh2igHbA09s4%2BclZ%2BsiY%3D&amp;ts=1644232065",
+  },
+  {
+    title: `반갑다, RE100 논란.. 자본주의자가 꼭 알아야 할 지금 벌어지는 일들 [탄소 후 미래]`,
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/ohmynews/20220207152103030sods.jpg",
+  },
+  {
+    title: `안갯속, 시민의 열망만 빛난다[미얀마 쿠데타 1년]`,
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/hani21/20220207112812874lszn.jpg",
+  },
+  // {
+  //   title: `'뇌피셜' 그만! AI가 밝힌 '세계서 가장 아름다운 장소'의 정체`,
+  //   thumb:
+  //   "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/mk/20220207180302837clkg.jpg",
+  // },
+  // {
+  //   title: `"옆집 살아도 이웃되려나"..文 양산 사저 현장 가보니`,
+  //   thumb:
+  //     "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/kukminilbo/20220207171059104jxxi.jpg",
+  // },
+  // {
+  //   title: `"옆집 살아도 이웃되려나"..文 양산 사저 현장 가보니`,
+  //   thumb:
+  //     "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/kukminilbo/20220207171059104jxxi.jpg",
+  // },
+  // {
+  //   title: `"옆집 살아도 이웃되려나"..文 양산 사저 현장 가보니`,
+  //   thumb:
+  //     "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/kukminilbo/20220207171059104jxxi.jpg",
+  // },
+  // {
+  //   title: `"옆집 살아도 이웃되려나"..文 양산 사저 현장 가보니`,
+  //   thumb:
+  //     "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/kukminilbo/20220207171059104jxxi.jpg",
+  // },
+  // {
+  //   title: `"옆집 살아도 이웃되려나"..文 양산 사저 현장 가보니`,
+  //   thumb:
+  //     "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/kukminilbo/20220207171059104jxxi.jpg",
+  // },
+];
+
+export const newsList = [
+  `"정은경 청장, 여당 후보 찍도록 코로나 잘 관리해달라"..민주당 김성주 의원, 야당 반발하자 사과`,
+  `윤석열 검찰총장 때 김만배 "욕하고 싸워"..尹 "10년 넘게 차 한잔 안 했다"`,
+  `강북 빠지는데 강남은 철옹성..집값 격차 벌어진다`,
+  `전기차 국내 양강 '현대차 vs 테슬라'..올해 성패 가르는 건 '보조금'`,
+  `국민의힘 종로 '전략공천' 결정.."조은희 5% 패널티"`,
+  `수원지검, 성남지청에 '성남FC 의혹' 보완 수사 지휘`,
+  `'부산 E컵' 아시나요?[뭐라노]`,
+  `윤석열 당선 가능성 역전.."김혜경 의혹 등 영향"[JTBC 여론조사]`,
+  `암 위험 높이는 '최악의 음료'는?`,
+  `'벤츠 세상'이라더니 또 혼쭐났다..'겹경사' BMW, 7년만에 1위 노린다[왜몰랐을카]`,
+  `풍성한 저녁 기대했더니..유통기한 지난 제주  한우`,
+  `정은경 "내달초 재택치료 100만명"..일반확진자는 사실상 손놔`,
+  `연준 22년 만의 '빅샷' 확률, 30%도 넘었다..&nbsp;亞 증시 일제히 약세`,
+  `권은희"尹 단일화 테이블에 가져올 건 소맥 뿐, 安 술 전혀 안 해"[정면승부]`,
+  ` 與 "母제사준비 직원동원? 김혜경씨 아닌 이 후보가 부탁한 것"`,
+];
+
+export const tv = [
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/daumtop_chanel/tromm/20220207095525509.jpg",
+    desc: "남편에게 맞은 딸에게 아버지가 한 말",
+  },
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/daumtop_chanel/tromm/20220207051945392.jpg",
+    desc: "아이 가진 박하선에게 친정 엄마가 해준 얘기",
+  },
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/daumtop_chanel/tromm/20220207012803522.jpg",
+    desc: "늦기 전에 둘째 생각해보라는 시어머니",
+  },
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/daumtop_chanel/tromm/20220207104817077.jpg",
+    desc: "임신 눈치챈 직장 상사의 한마디",
+  },
+];
+
+export const cafe = [
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=http://t1.daumcdn.net/brunch/service/guest/image/lVdCvzb99InYL_eikYm7EHKv_LU.png",
+    title: "프롤로그",
+    teller: "by 일드에서 배우는 직장인 생활백서",
+  },
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=http://t1.daumcdn.net/brunch/service/user/2Loz/image/9mA_gRYf6z7-QNjDMfFGxasefo0.jpg",
+    title: "1. 아침부터 저녁까지 그랩",
+    teller: "by 우리가 모르는 베트남 I",
+  },
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=http://t1.daumcdn.net/brunch/service/user/3YPB/image/N9-zziLIep8rkxm3O3jmHojG86I.jpg",
+    title: "듣고 싶은 말",
+    teller: "by 요즘 학교 요즘 애들 요즘 교사",
+  },
+  {
+    thumb:
+      "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=http://t1.daumcdn.net/brunch/service/user/4EOJ/image/zWsDACku6Hlz0F0OziZYdpvBnPM.JPG",
+    title: "들어가며",
+    teller: "by 나의 서른 살 대만",
+  },
+];
+
+export const channel = {
+  homeAndCook: [
+    {
+      thumb:
+        "https://t1.daumcdn.net/daumtop_deco/images/top/2017/bg_plate5.png",
+      item: "ASMR을 들으면 정말 잠이 잘 올까요?",
+      origin: "정신의학신문",
+    },
+    {
+      thumb:
+        "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/SNUH/202202070851311073h19.jpeg",
+      item: `최장암으로 가는 만성 췌장염, '이것' 때문에 생긴다!`,
+      origin: "서울대학교병원",
+    },
+    {
+      thumb:
+        "https://t1.kakaocdn.net/thumb/P153x93cc1/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F53220%2Fd5d64578353a66bfe611625621e77717.png",
+      item: `2분 만에 나를 알아보는 '무료 테스트`,
+      origin: "카카오같이가치",
+    },
+    {
+      thumb:
+        "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/ohouse/20220207180208998unpq.png",
+      item: `"집 짓기 9년 전, 땅부터 미리 사뒀어요" 50대 부부,`,
+      origin: "오늘의집",
+    },
+    {
+      thumb:
+        "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/07/harmony/20220207074202085fwof.png",
+      item: "완전 그사세인 연예인 출연료",
+      origin: "남녀생활백서",
+    },
+    {
+      thumb:
+        "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/news/202202/06/NOSearch/20220206115824866jmer.png",
+      item: "요리 못해도 괜찮아! OO가 다 해줘!",
+      origin: "노써치",
+    },
+    {
+      thumb:
+        "https://img1.daumcdn.net/thumb/S308x188a.q80/?fname=https://t1.daumcdn.net/section/oc/39d95f5c2cb449a5bc104c6b73e74284",
+      item: "명절에 남은 전, 모둠전 전골",
+      origin: "브런치 by 너구르르",
+    },
+  ],
+};
+
+export const best = [
+  {
+    thumb: "https://t1.daumcdn.net/daumtop_deco/images/top/2017/bg_plate9.png",
+    title: "주부 9단도 놀랄듯, 다이소 알바생이 뽑은 1위 주방 꿀템",
+  },
+  {
+    thumb: "https://t1.daumcdn.net/daumtop_deco/images/top/2017/bg_plate9.png",
+    title: "치마가 한쪽으로 잘 돌아가거나 다리를 잘 꼬면 생기는 병",
+  },
+  {
+    thumb: "",
+    title: "물티슈캡 있으세요? 그럼 바로 주방에 붙이세요. 모두가 놀랍니다",
+  },
+  { thumb: "", title: "아버님의 플렉스" },
+  { thumb: "", title: "알고 보니 자가격리가 체질" },
+  { thumb: "", title: `머리를 자주 감아도 비듬이 생기는 '의외의' 이유` },
+  { thumb: "", title: `간암에 걸리는 가장 큰 원인은 바로 '이것'` },
+];
+
+export const shoppingFlatformList = [
+  { flatform: "하프클럽", link: "" },
+  { flatform: "패션플러스", link: "" },
+  { flatform: "쿠팡", link: "" },
+  { flatform: "11번가", link: "" },
+  { flatform: "GS샵", link: "" },
+  { flatform: "리빙픽", link: "" },
+  { flatform: "깔끔대장", link: "" },
+  { flatform: "아이스탁몰", link: "" },
+  { flatform: "마이더스비", link: "" },
+  { flatform: "매거진톡", link: "" },
+  { flatform: "리린", link: "" },
+  { flatform: "뮬리안", link: "" },
+  { flatform: "모놀로그", link: "" },
+  { flatform: "조아맘", link: "" },
+  { flatform: "젝시믹스", link: "" },
+  { flatform: "캔마트", link: "" },
+  { flatform: "지젤", link: "" },
+];
+
+export const shoppingItems = [
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F176487%2F68bc12f668f25c2dd628e742521ace60.jpg&TWGServiceId=b2&Expires=1644336000&Signature=Nu40rasbWvKSNgyiZiHj8%2BZ5syI%3D",
+    item: "다들놀라는가성비 정말 단돈만원!",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F279031%2F2024ab3e23f08e36efca1d72b2a3f95c.jpg&TWGServiceId=b2&Expires=1644336000&Signature=AQg787RS%2Bq8rQ4c3hW7KRnWE6GY%3D",
+    item: "소/장/필/수 최대80%특가!",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F261807%2F9415f87675dad81c48a5c0de4fa96d3f.jpg&TWGServiceId=b2&Expires=1644336000&Signature=GDuQtzXmgwSnBwMRLAVFPQYOj0s%3D",
+    item: "+스톤아일랜드+ 보복소비 제대로",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F219377%2Fc09c43a69de6cc72a573a179be3f0707.jpg&TWGServiceId=b2&Expires=1644336000&Signature=IUC5fOMFFAkM%2FylGSlusgu3Pcd8%3D",
+    item: "강철 고무장갑 5종6,900원",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F154184%2F9c5399c1eb73ccd9043a1b82d6962da0.jpg&TWGServiceId=b2&Expires=1644336000&Signature=YzfF9rxQzsM8VCWBOTKisub7c64%3D",
+    item: "BEST 봄슈즈 스틸레토플랫슈즈",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F154181%2F6938eb9db46912f59d624d2240a0755c.jpg&TWGServiceId=b2&Expires=1644336000&Signature=1f2anFI045miP0RMlRWh5URJHv8%3D",
+    item: "한번먹기시작하면 계속 들어가요~",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F154124%2Feffeea750e80a5be97a00b1f6f17d2d0.jpg&TWGServiceId=b2&Expires=1644336000&Signature=oXTpul0A6QqnH8CvMaXZSXBPxrU%3D",
+    item: "백년묵은 각질 사르르~ 시원해",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F350752%2F19c9a29dd10ff63eb2f224fa8d47c466.jpg&TWGServiceId=b2&Expires=1644336000&Signature=wmOc3zbncdjbXy7AdUwueU1aFdg%3D",
+    item: "엘칸토 염소가죽 2만원대무료배송",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F153427%2Fa35f9dfccdc9bc76e153059881f783f2.jpg&TWGServiceId=b2&Expires=1644336000&Signature=eYNn%2B4OO2UiB4CasQMN25wEEWFw%3D",
+    item: "골프브랜드룩 21만→7만~",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F153785%2F042e5fc4bf178e1c0df88e10c04666e8.jpg&TWGServiceId=b2&Expires=1644336000&Signature=de0g6m14BRB7FfYQshUIojlp%2FlM%3D",
+    item: "쫄깃한식감환상! 이귀한게만원대?",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F293799%2F75670f535c0f7468a39522437369f3c0.jpg&TWGServiceId=b2&Expires=1644336000&Signature=JJOI9xTQojyEpSNfO%2BS%2BwWszM0s%3D",
+    item: "~50% 특가 소가죽 크로스백",
+  },
+  {
+    picture:
+      "https://img1.daumcdn.net/thumb/S172x208/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F149993%2F56e5abc96fc2103b9014a9e9370b607f.jpg&TWGServiceId=b2&Expires=1644336000&Signature=VPfeNUgQUJsFi6b%2BYdhw%2Fd2GRcs%3D",
+    item: "스포츠 브랜드 최대 92%할인",
+  },
+];
+
+export const hotdealList = [
+  {
+    thumb:
+      "https://shop4.daumcdn.net/thumb/C220x120/?fname=http%3A%2F%2Fshop4.daumcdn.net%2Fshophow%2Fsib%2F0_220208154119_PSIoNLgXCRqjlMoFQoatdJpiBZXbQUvg&scode=shdefault2",
+    item: "트렌디한 감성을 담은 여성의류 SPA 브랜드 #미쏘",
+  },
+  {
+    thumb:
+      "https://shop2.daumcdn.net/thumb/C220x120/?fname=http%3A%2F%2Fshop2.daumcdn.net%2Fshophow%2Fsib%2F0_220208154123_EhpJbeZsriDDIAsZNDZJNmYREimuELaD&scode=shdefault2",
+    item: "조합하는 재미! 다양한 매치가 가능한 레이어드 반지",
+  },
+  {
+    thumb:
+      "https://shop1.daumcdn.net/thumb/C220x120/?fname=http%3A%2F%2Fshop3.daumcdn.net%2Fshophow%2Fsib%2F0_220124165917_PJSdUBFclcHGWOtxBCBFvTNOIVctzjvn&scode=shdefault2",
+    item: "운동 후의 뻐근함을 시원하게 풀어줄 마사지 도구 모음",
+  },
+  {
+    thumb:
+      "https://shop3.daumcdn.net/thumb/C220x120/?fname=http%3A%2F%2Fshop3.daumcdn.net%2Fshophow%2Fsib%2F0_220124165921_STdRhnCuBWPLssmDEJQMLdxcuTyxDYaB&scode=shdefault2",
+    item: "아이의 창의력과 정서발달에 도움을 주는 역할놀이",
+  },
+  {
+    thumb:
+      "https://shop2.daumcdn.net/thumb/C220x120/?fname=http%3A%2F%2Fshop4.daumcdn.net%2Fshophow%2Fsib%2F0_220124165924_nDMjWFfPuMmdUUKfqwrrsgbHLIYVKKwr&scode=shdefault2",
+    item: "내 몸에 맞춘 최적의 각도를 찾을 수 있게, 모션베드",
+  },
+];
